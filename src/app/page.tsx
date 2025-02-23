92d9bb0de85c0a86d88e92c939207646bda5b9a6
Zach <thereezen24@gmail.com>
2025-02-23 02:54:55 -0500

    back to original

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import { Slide } from "react-awesome-reveal";
 export default function Home() {
   return (
     <div className="flex flex-col justify-center items-center min-h-screen gap-2 lg:flex-row">
-        <div>raw<Projects/></div>
+        <div><Projects/></div>
         <div className="flex flex-col"><Socials/><Experience/><CustomChatBox/></div>
         <div className="lg:w-[235px] w-[320px]"><Accordian title="Towson University" subtext="Bachelor of Science, Computer Science." description=" Courses: Data Structures & Algorithms, Calculus 1 & 2, Discrete Mathematics" dateStarted={2022} dateFinished={2026} gpa={3.088}/><Skills/></div>
     </div>
